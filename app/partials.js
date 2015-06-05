d83ab19264b20e3be0e73a677224ec234a761903
Phillip Friedrich <friedrich.phillip@gmail.com>
2015-06-05 12:43:53 +0200

    remove auto generated content

--- app/partials.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="references.ts" />
-'use strict';
-define(["require", "exports", 'angular'], function (require, exports, angular) {
-    exports.partialsModule = angular.module('partials', []);
-});
-/* inject:partials */ /* endinject */
-
-//# sourceMappingURL=partials.js.map
